62cf8edbe5434e9cdd41970c39b88cb18c2ded93
JJ Ryan <jryan@motor.com>
2017-06-02 21:13:53 -0400

    Wow that does the same thing in 10 lines of code

--- DateProject/Program.cs
+++ DateProject/Program.cs
@@ -8,84 +8,13 @@ namespace DateProject
 {
     class Program
     {
-        static int[] inputToArray (string input)
-        {
-            int date = Convert.ToInt32(input);
-            int[] d1array = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-            int counter = 0;
-            while (counter < 8)
-            {
-                d1array[counter] = date % 10;
-                date = date / 10;
-                counter++;
-            }
-            return d1array;
-        }
-        static int getMonth(int monthValue)
-        {
-            switch (monthValue)
-            {
-                case 1:
-                    monthValue = 0;
-                    break;
-                case 2:
-                    monthValue = 31;
-                    break;
-                case 3:
-                    monthValue = 31 + 28;
-                    break;
-                case 4:
-                    monthValue = 31 + 28 + 31;
-                    break;
-                case 5:
-                    monthValue = 31 + 28 + 31 + 30;
-                    break;
-                case 6:
-                    monthValue = 31 + 28 + 31 + 30 + 31;
-                    break;
-                case 7:
-                    monthValue = 31 + 28 + 31 + 30 + 31 + 30;
-                    break;
-                case 8:
-                    monthValue = 31 + 28 + 31 + 30 + 31 + 30 + 31;
-                    break;
-                case 9:
-                    monthValue = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-                    break;
-                case 10:
-                    monthValue = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-                    break;
-                case 11:
-                    monthValue = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-                    break;
-                case 12:
-                    monthValue = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
-                    break;
-                    //                default:
-                    //                    Console.WriteLine("Error in input");
-                    //                    break;
-            }
-            return monthValue;
-        }
+        
         static void Main()
         {
-            string input1 = "01012017";
-            string input2 = "02112011";
-            int [] d1array = inputToArray(input1);
-            int [] d2array = inputToArray(input2);
-            int year1 = d1array[0] + (d1array[1] * 10) + (d1array[2] * 100) + (d1array[3] * 1000); // Console.WriteLine(year1);
-            int year2 = d2array[0] + (d2array[1] * 10) + (d2array[2] * 100) + (d2array[3] * 1000); // Console.WriteLine(year2);
-            int day1 = d1array[4] + (d1array[5] * 10); // Console.WriteLine(day1);
-            int day2 = d2array[4] + (d2array[5] * 10); // Console.WriteLine(day2);
-            int month1 = d1array[6] + (d1array[7] * 10); // Console.WriteLine(month1);
-            int month2 = d2array[6] + (d2array[7] * 10); // Console.WriteLine(month2);
-            int monthdays1 = getMonth(month1);
-            int monthdays2 = getMonth(month2);
-           // Console.WriteLine(monthdays1);
-           //  Console.WriteLine(monthdays2);
-            int totalDate1 = (year1 * 365) + monthdays1 + day1;
-            int totalDate2 = (year2 * 365) + monthdays2 + day2;
-            int diffDate = Math.Abs(totalDate1 - totalDate2);
+            DateTime dtToday = new System.DateTime(2012, 6, 2, 0, 0, 0);
+            DateTime dtMonthBefore = new System.DateTime(2012, 5, 2, 0, 0, 0);
+            TimeSpan diffResult = dtToday.Subtract(dtMonthBefore);
+            int diffDate = Convert.ToInt32(diffResult.TotalDays);
             int yearsDiff = diffDate / 365;
             int monthsDiff = (diffDate - (yearsDiff*365))/30;
             int daysDiff = (diffDate - (yearsDiff * 365) - (monthsDiff * 30));
