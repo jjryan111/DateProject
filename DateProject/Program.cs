f52b66ae0a32caaf341c50376be864f0129fe065
JJ Ryan <jryan@motor.com>
2017-06-03 21:13:39 -0400

    Finished?

--- DateProject/Program.cs
+++ DateProject/Program.cs
@@ -21,45 +21,85 @@ namespace DateProject
             }
             return d1array;
         }
-
-        static int getYear(int [] yearArray)
+        static int getYear(int[] yearArray)
         {
             int year = (yearArray[3] * 1000) + (yearArray[2] * 100) + (yearArray[1] * 10) + yearArray[0];
             return year;
         }
-
-        static int getMonth (int [] monthArray)
+        static int getMonth(int[] monthArray)
         {
             int month = (monthArray[7] * 10) + monthArray[6];
             return month;
         }
-
-        static int getDay (int [] dayArray)
+        static int getDay(int[] dayArray)
         {
             int day = (dayArray[5] * 10) + dayArray[4];
             return day;
         }
+        static int [] dateCheck()
+        {
+            bool badInput = true;
+            string checkString = "";
+            int[] checkArray = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            while (badInput)
+            {
+                badInput = false;
+                Console.Write("Enter in a date as MMDDYYYY: ");
+                checkString = Console.ReadLine();
+                try
+                {
+                    Convert.ToInt32(checkString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hey! That's not a date! Try again.");
+                    badInput = true;
+                }
+                if(!badInput)
+                {
+                    int inputNum = Convert.ToInt32(checkString);
+                    if((inputNum > 12319999) || (inputNum <1010001))
+                    {
+                        Console.WriteLine("Hey! That's not a date! Try again.");
+                        badInput = true;
+                    }
+                    else
+                    {
+                        checkArray = inputToArray(checkString);
+                        if ((getDay(checkArray) > 31) || (getDay(checkArray) < 1))
+                        {
+                            badInput = true;
+                            Console.WriteLine("How many days in a month?");
+                        }
+                        if (getDay(checkArray) > 30 && (getMonth(checkArray)== 4 || getMonth(checkArray) == 6 || getMonth(checkArray) == 9 || getMonth(checkArray) == 11))
+                        {
+                            badInput = true;
+                            Console.WriteLine("Thirty days has September, April, June and November!");
+                        }
+                        if (getMonth (checkArray) == 2 && (getDay(checkArray) > 28))
+                        {
+                            badInput = true;
+                            Console.WriteLine("February only has 28 days.");
+                        }
+                    }
+                   
+                }
 
+            }
+            return checkArray;
+        }
         static void Main()
         {
-            string input1 = "07032017";
-            string input2 = "06022017";
-            int[] inputarray1 = inputToArray(input1);
-            int[] inputarray2 = inputToArray(input2);
-
+            int [] inputarray1 = dateCheck();
+            int [] inputarray2 = dateCheck();
             DateTime firstDate = new System.DateTime(getYear(inputarray1), getMonth(inputarray1), getDay(inputarray1), 0, 0, 0);
             DateTime secondDate = new System.DateTime(getYear(inputarray2), getMonth(inputarray2), getDay(inputarray2), 0, 0, 0);
-            Console.WriteLine(firstDate);
-            Console.WriteLine(secondDate);
             TimeSpan diffResult = firstDate - secondDate;
-            int diffDate = Convert.ToInt32(diffResult.TotalDays);
+            int diffDate = Convert.ToInt32(Math.Abs(diffResult.TotalDays));
             int yearsDiff = diffDate / 365;
             int monthsDiff = (diffDate - (yearsDiff*365))/30;
             int daysDiff = (diffDate - (yearsDiff * 365) - (monthsDiff * 30));
-            Console.WriteLine(diffDate);
-            Console.WriteLine(yearsDiff);
-            Console.WriteLine(monthsDiff);
-            Console.WriteLine(daysDiff);
+            Console.Write("The difference is {0} years, {1} months, and {2} days.",yearsDiff,monthsDiff,daysDiff);
             Console.Read();
         }
     }
